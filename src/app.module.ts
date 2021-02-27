cf0844c587e2cf0ab253ad75e91db7633a727ef4
mtkozlowski <kooziu@gmail.com>
2021-02-27 14:40:09 +0100

    [feat] add TypeORM to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,16 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AuthService } from './auth/auth.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MoviesModule, ConfigModule.forRoot(), AuthModule, UsersModule],
+  imports: [
+    MoviesModule,
+    ConfigModule.forRoot(),
+    AuthModule,
+    UsersModule,
+    TypeOrmModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
