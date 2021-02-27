378ad6ee8b8dbc5ed86c04cc3529731bf8042b6b
mtkozlowski <kooziu@gmail.com>
2021-02-27 19:27:19 +0100

    [feat] remove bcrypt from auth.service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { User, UsersService } from '../users/users.service';
-import * as bcrypt from 'bcrypt';
 import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
@@ -11,7 +10,7 @@ export class AuthService {
 
   async validateUser(username: string, password: string): Promise<any> {
     const user = await this.usersService.findOne(username);
-    if (user && bcrypt.compare(password, user.password)) {
+    if (user && password === user.password) {
       const { password, ...result } = user;
       return result;
     }
