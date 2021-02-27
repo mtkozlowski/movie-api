57cf75bf3ff8fd1bb7d107b6cabd2c6b47d7fb0a
mtkozlowski <kooziu@gmail.com>
2021-02-27 12:07:46 +0100

    [refact] move movie class to src folder

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Get, Post, Query } from '@nestjs/common';
-import { Movie } from 'model/movie';
+import { Movie } from '../models/movie';
 import { MoviesService } from './movies.service';
 
 @Controller('movies')
 export class MoviesController {
   constructor(private readonly movieService: MoviesService) {}
 
+  //Token from Header
   @Get()
   index(@Query('token') token: string): any {
     return token;
