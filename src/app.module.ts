2eef459e929c3fa0f16b6629bd2d0f99a168b40c
mtkozlowski <kooziu@gmail.com>
2021-02-27 13:55:21 +0100

    [feat] add login to app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MoviesModule } from './movies/movies.module';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { AuthService } from './auth/auth.service';
 
 @Module({
-  imports: [MoviesModule, ConfigModule.forRoot()],
+  imports: [MoviesModule, ConfigModule.forRoot(), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
