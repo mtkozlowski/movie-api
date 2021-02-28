626a4bb775455ac47caa233e524a94a8db0dd24e
mtkozlowski <kooziu@gmail.com>
2021-02-28 23:51:21 +0100

    [fix] add RoleModule to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OmdbModule } from './Omdb/omdb.module';
+import { RoleModule } from './role/role.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { OmdbModule } from './Omdb/omdb.module';
     UsersModule,
     TypeOrmModule.forRoot(),
     OmdbModule,
+    RoleModule,
   ],
   controllers: [AppController],
   providers: [AppService],
